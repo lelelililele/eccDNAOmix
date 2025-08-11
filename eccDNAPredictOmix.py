1f5b32b8450948c367efe6f4ebd46b54f926e19e
lelelililele <38709888+lelelililele@users.noreply.github.com>
2025-08-11 10:02:22 +0800

    Update eccDNAPredictOmix.py

--- eccDNAPredictOmix.py
+++ eccDNAPredictOmix.py
@@ -16,7 +16,6 @@ from models import DeepMultimodalModel
 from train_utils import train_model
 from eval_utils import evaluate_modality, final_metrics
 from feature_extract import FeatureExtractor
-from umap_vis import visualize_umap
 
 device = torch.device("cpu")
 
