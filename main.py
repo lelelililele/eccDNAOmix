18b45776f27fbe42ac83c687377c3ae43efcc320
lelelililele <38709888+lelelililele@users.noreply.github.com>
2025-08-11 10:01:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from models import DeepMultimodalModel
 from train_utils import train_model
 from eval_utils import evaluate_modality, final_metrics
 from feature_extract import FeatureExtractor
-from umap_vis import visualize_umap
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
