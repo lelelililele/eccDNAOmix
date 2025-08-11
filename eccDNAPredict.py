55d50699661930822dfd8919c7398305e7d8f83c
lelelililele <38709888+lelelililele@users.noreply.github.com>
2025-08-11 10:02:06 +0800

    Update eccDNAPredict.py

--- eccDNAPredict.py
+++ eccDNAPredict.py
@@ -21,7 +21,6 @@ from models import DeepMultimodalModel
 from train_utils import train_model
 from eval_utils import evaluate_modality, final_metrics
 from feature_extract import FeatureExtractor
-from umap_vis import visualize_umap
 #import seaborn as sns
 from scipy.stats import spearmanr
 from sklearn.manifold import MDS  # 新增导入
